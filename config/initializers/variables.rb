8e93f7827567117155a749a391fd8454ed8df872
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-07-23 10:54:21 +0700

    add article category on init

--- config/initializers/variables.rb
+++ config/initializers/variables.rb
@@ -1,12 +1,24 @@
 ARTICLE_TYPES = [{
-	:id => 1,
-	:type => "Tutorial"
+		:id => 1,
+		:type => "Tutorial"
 	},
   {
 	  :id => 2,
 	  :type => "My Thoughts"
   },
   {
-	:id => 3,
-	:type => "Family"
-}]
+	  :id => 3,
+	  :type => "Parenting"
+  },
+  {
+	  :id => 4,
+	  :type => "My Kids"
+	},
+	{
+		:id => 5,
+		:type => "Me"
+	},
+	{
+		:id => 6,
+		:type => "My Husband"
+	}]
