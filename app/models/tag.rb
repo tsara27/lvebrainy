fc37667160e79c9b93462663342d100e691e5a2d
Tsara Fatma Larasati S <tsara.tf@gmail.com>
2014-09-12 12:20:21 +0700

    fix tag validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
 	has_and_belongs_to_many :articles
 
-	validates :tag_name, uniqueness: true, allow_nil: true
+	validates :tag_name, uniqueness: true, presence: true
 end
